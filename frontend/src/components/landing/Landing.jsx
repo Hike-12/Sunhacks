3df5c90b56bd56cefb655c4882ecbbc3c89cae39
Reniyas717 <reniyasnadar717@gmail.com>
2025-08-22 03:43:08 +0530

    fc

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "../../context/ThemeContext";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import FeaturesGrid from "./FeaturesGrid";
-import VideoSection from "./VideoSection";
+// import VideoSection from "./VideoSection";
 import TestimonialsMarquee from "./TestimonialsMarquee";
 import CTASection from "./CTASection";
 import Footer from "./Footer";
